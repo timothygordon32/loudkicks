ba55b4f1ca0cc3e59a75d2016f229d529a752af4
Tim Gordon <timothygordon32@gmail.com>
2015-04-09 14:12:46 +0100

    Add pending read of aging post scenario.

--- src/test/scala/features/ReadingFeature.scala
+++ src/test/scala/features/ReadingFeature.scala
@@ -24,6 +24,21 @@ class ReadingFeature extends FeatureSpec with GivenWhenThen with Matchers {
         timeLine.lines should contain ("I love the weather today")
       }
     }
+
+    scenario("Alice reads her own single posted message from 10 seconds ago") {
+      new WithApp {
+        Given("Alice has posted 'I love the weather today' ten seconds ago")
+        app.parse("Alice -> I love the weather today") should
+          be(Posted(Post(User("Alice"), Message("I love the weather today"))))
+
+        When("Alice's time line is read")
+        val timeLine = app.parse("Alice")
+
+        Then("the post 'I love the weather today (10 seconds ago)' should be listed")
+        pending
+        timeLine.lines should contain ("I love the weather today (10 seconds ago)")
+      }
+    }
   }
 
   trait WithApp {
