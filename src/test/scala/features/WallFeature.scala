0ffbf5a2576b8b94af379c43ea145f68dff34f57
Tim Gordon <timothygordon32@gmail.com>
2015-04-11 13:05:47 +0100

    Passed read own posts on own wall scenario.

--- src/test/scala/features/WallFeature.scala
+++ src/test/scala/features/WallFeature.scala
@@ -7,21 +7,23 @@ import org.loudkicks.{Message, User, Post}
 class WallFeature extends AcceptanceSpec {
 
   info("As a social networking user")
-  info("I want a wall of all my posts and of all the users I follow")
+  info("I want to see a wall of all my posts and of all the users I follow")
   info("So that I can see what me and my friends have been up to all in one place")
 
-  scenario("User's own posts appear on their wall") (pendingUntilFixed {
+  scenario("User's own posts appear on their wall") {
     new WithApp {
       Given("Charlie has posted 'I'm in New York today! Anyone want to have a coffee?' 2 seconds ago")
-      timeIs(inThePast(2.seconds))
-      app.parse("Alice -> I love the weather today") should
-        be(Posted(Post(User("Alice"), Message("I love the weather today"), inThePast(2.seconds))))
+      in(thePast(2.seconds))
+      app.parse("Charlie -> I'm in New York today! Anyone want to have a coffee?") should
+        be(Posted(Post(User("Charlie"), Message("I'm in New York today! Anyone want to have a coffee?"), thePast(2.seconds))))
 
       When("Charlie looks at his own wall")
+      in(thePresent)
       val wall = app.parse("Charlie wall")
 
       Then("the wall shows the message 'Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)'")
+      wall.lines should have size 1
       wall.lines should contain ("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)")
     }
-  })
+  }
 }
