fcc93a17300696eb75ec09eeb654eadecc94d873
Tim Gordon <timothygordon32@gmail.com>
2015-04-11 14:07:59 +0100

    Add pending scenario for wall showing other user's post.

--- src/test/scala/features/WallFeature.scala
+++ src/test/scala/features/WallFeature.scala
@@ -17,13 +17,42 @@ class WallFeature extends AcceptanceSpec {
       app.parse("Charlie -> I'm in New York today! Anyone want to have a coffee?") should
         be(Posted(Post(User("Charlie"), Message("I'm in New York today! Anyone want to have a coffee?"), thePast(2.seconds))))
 
-      When("Charlie looks at his own wall")
+      When("Charlie's wall is shown")
       in(thePresent)
       val wall = app.parse("Charlie wall")
 
       Then("the wall shows the message 'Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)'")
       wall.lines should have size 1
       wall.lines should contain ("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)")
+    }
+  }
+
+  scenario("Following one other user will show both user's own and other user's posts on the their wall") {
+    new WithApp {
+      pending
+
+      Given("Charlie has posted 'I'm in New York today! Anyone want to have a coffee?' 2 seconds ago")
+      in(thePast(2.seconds))
+      app.parse("Charlie -> I'm in New York today! Anyone want to have a coffee?") should
+        be(Posted(Post(User("Charlie"), Message("I'm in New York today! Anyone want to have a coffee?"), thePast(2.seconds))))
+
+      And("Alice has posted 'I love the weather today' 5 minutes ago")
+      in(thePast(5.minutes))
+      app.parse("Alice -> I love the weather today") should
+        be(Posted(Post(User("Alice"), Message("I love the weather today"), thePast(5.minutes))))
+
+      And("Charlie is following Alice")
+      app.parse("Charlie follows Alice")
+
+      When("Charlie's wall is shown")
+      in(thePresent)
+      val wall = app.parse("Charlie wall")
+
+      Then("the wall shows the first message 'Charlie has posted 'I'm in New York today! Anyone want to have a coffee?' (2 seconds ago)'")
+      wall.lines(0) should be("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)")
+      And("the wall shows the second message 'Alice - I love the weather today (5 minutes ago)'")
+      wall.lines(0) should be("Alice - I love the weather today (5 minutes ago)")
+      wall.lines should have size 2
     }
   }
 }
