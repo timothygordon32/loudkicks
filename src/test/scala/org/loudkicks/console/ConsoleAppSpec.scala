d1693d6427eabe8357c3290ac4352668d0c49926
Tim Gordon <timothygordon32@gmail.com>
2015-04-12 17:37:06 +0100

    Clarify console app tests.

--- src/test/scala/org/loudkicks/console/ConsoleAppSpec.scala
+++ src/test/scala/org/loudkicks/console/ConsoleAppSpec.scala
@@ -6,38 +6,50 @@ import org.loudkicks.UnitSpec
 
 class ConsoleAppSpec extends UnitSpec {
 
-  "ConsoleApp" should {
+  trait TestConsoleApp extends ConsoleApp {
+    val outputCapture = new ByteArrayOutputStream()
+    val output = new PrintWriter(new PrintStream(outputCapture))
+  }
 
-    "output a prompt each time app is ready for input" in {
-      val outputCapture = new ByteArrayOutputStream()
-      val app = new ConsoleApp {
+  "ConsoleApp" when {
+    "started" should {
+      "output a prompt initially" in new TestConsoleApp {
         val prompt = "> "
-        val output = new PrintWriter(new PrintStream(outputCapture))
+        val input = Seq.empty.toIterator
+
+        def parse(line: String) = Empty
+
+        run()
+
+        outputCapture.toString should be("> ")
+      }
+
+      "output a prompt each time app is ready for input" in new TestConsoleApp {
+        val prompt = "> "
         val input = Seq("anything").toIterator
 
         def parse(line: String) = Empty
+
+        run()
+
+        outputCapture.toString should be("> > ")
       }
-
-      app.run()
-
-      outputCapture.toString should be ("> > ")
     }
 
-    "output response from parsed input" in {
-      val outputCapture = new ByteArrayOutputStream()
-      val app = new ConsoleApp {
+    "parsing a line of input" should {
+      "output response from parsed input" in new TestConsoleApp {
         val prompt = ""
-        val output = new PrintWriter(new PrintStream(outputCapture))
         val input = Seq("anything").toIterator
+
         def parse(line: String) = Lines(Seq("Output line 1", "Output line 2"))
-      }
 
-      app.run()
+        run()
 
-      outputCapture.toString should be
+        outputCapture.toString should be
         """Output line 1
           |Output line 2
-          |""".stripMargin
+          | """.stripMargin
+      }
     }
   }
 }
