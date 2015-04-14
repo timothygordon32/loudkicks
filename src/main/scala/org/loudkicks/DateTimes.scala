a90dd5536e4ee7f359078ff4bfd2a6c211b799c8
Tim Gordon <timothygordon32@gmail.com>
2015-04-14 01:12:17 +0100

    Remove unnecessary implicit conversion.

--- src/main/scala/org/loudkicks/DateTimes.scala
+++ src/main/scala/org/loudkicks/DateTimes.scala
@@ -5,7 +5,5 @@ import java.util.Date
 import org.joda.time.{DateTime, DateTimeZone}
 
 object DateTimes {
-  implicit def toDate(dateTime: DateTime): Date = dateTime.toDate
-
   def now = DateTime.now(DateTimeZone.UTC)
 }
--- src/main/scala/org/loudkicks/console/EventFormatting.scala
+++ src/main/scala/org/loudkicks/console/EventFormatting.scala
@@ -1,7 +1,6 @@
 package org.loudkicks.console
 
 import org.joda.time.DateTime
-import org.loudkicks.DateTimes._
 import org.loudkicks.service.TimeSource
 import org.ocpsoft.prettytime.PrettyTime
 import org.ocpsoft.prettytime.units.JustNow
@@ -13,12 +12,12 @@ trait EventFormatting {
     val ResolutionAtSeconds = 999
 
     private val formatter = {
-      val pt = new PrettyTime(from)
+      val pt = new PrettyTime(from.toDate)
       pt.getUnit(classOf[JustNow]).setMaxQuantity(ResolutionAtSeconds)
       pt
     }
 
-    def format(when: DateTime): String = formatter.format(when)
+    def format(when: DateTime): String = formatter.format(when.toDate)
   }
 
   def format(time: DateTime): String = EventFormatter(from = timeSource.now).format(time)
